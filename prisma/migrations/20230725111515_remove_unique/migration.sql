13370967e59192c03b3737d41b155741ca6a55ed
Diego Massarini <63283003+webdiego@users.noreply.github.com>
2023-07-25 13:15:43 +0200

    remove unique

---
+++ prisma/migrations/20230725111515_remove_unique/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropIndex
+DROP INDEX "User_email_key";
+
+-- DropTable
+DROP TABLE "Post";
