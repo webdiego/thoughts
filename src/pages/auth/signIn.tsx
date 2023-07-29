433f4833cb45ed7a82948f9468c093753a992cac
Diego Massarini <63283003+webdiego@users.noreply.github.com>
2023-07-29 21:34:48 +0200

    fix: add base url env

--- src/pages/auth/signIn.tsx
+++ src/pages/auth/signIn.tsx
@@ -21,7 +21,7 @@ export default function SignIn({
               <button
                 onClick={() =>
                   signIn('google', {
-                    callbackUrl: 'http://localhost:3000/dashboard',
+                    callbackUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/dashboard`,
                   })
                 }
                 className="px-4 py-2 border flex gap-2 border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150 bg-white"
