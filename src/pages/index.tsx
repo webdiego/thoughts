a19fdb25625caf9408c2252356bec6b9ca4bd30b
Diego Massarini <63283003+webdiego@users.noreply.github.com>
2023-07-29 16:25:44 +0200

    trigger deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { useSession } from 'next-auth/react'
 import Navbar from '@components/Shared/Navbar'
 import Hero from '@components/Hero'
 import Feature from '@components/Feature'
@@ -7,6 +6,7 @@ import Footer from '@components/Shared/Footer'
 import { authOptions } from '../pages/api/auth/[...nextauth]'
 import { getServerSession } from 'next-auth/next'
 import { Session } from 'next-auth'
+
 export default function Home({ newSession }: { newSession: Session | null }) {
   let session = newSession
   return (
